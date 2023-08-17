361a0cd68f757af77d9fce8c37a370dcd07a8d08
bala <bmthedev@gmail.com>
2023-08-18 03:44:31 +0530

    writing a file

--- 1_Node Module System/4_fs.js
+++ 1_Node Module System/4_fs.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 
 //reading a file
-let file = fs.readFileSync('f1.txt')
-console.log("Output -> " + file);
+// let file = fs.readFileSync('f1.txt')
+// console.log("Output -> " + file);
+
+//writing a file
+fs.writeFileSync('f2.txt',' adding this through WRITEFILESYNC')     //this overwrites the file
+console.log('file written');
